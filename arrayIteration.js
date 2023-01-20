66bc540f88b22166eeef45351eef479f038c51b7
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-01-20 09:16:40 -0800

    Added some() and every() to arrayIteration

--- arrayIteration.js
+++ arrayIteration.js
@@ -12,6 +12,12 @@ const evens = ints.filter( n => n % 2 === 0).forEach( (n, index) => console.log(
 //returns a true boolean value if any of the values inside the array are true
 // ints = [-3, 3, -2, 2, -1, 1, 0];
 ints = [0, 1, 2, 3, 4, 5];
-const hasNegativeNums = (ints.some( (x, index, array) => x < 0) ? `Array has negative numbers in it.`: `Array does not have negative numbers in it.`)
+const hasNegativeNums = (ints.some( (x, index, array) => x < 0) ? `Array has negative numbers in it.`: `Array does not have negative numbers in it.`);
 console.log(hasNegativeNums);
 
+// every
+// every() is used to check whether all the elements any of the values inside the array are true
+ints = [-3, 3, -2, 2, -1, 1, 0];
+// ints = [1, 2, 3, 4, 5];
+const allArePositive = (ints.every( x => x > 0) ? "All numbers are positive!" : "All numbers are not positive. :\`(" );
+console.log(allArePositive);
